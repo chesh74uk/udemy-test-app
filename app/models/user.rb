0631101023c739eee59fca8ca5de36f5b4f9cfb3
Chesh <chesh@chesh.info>
2020-08-19 10:49:52 +0000

    added stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    before_save { self.email = email.downcase }
     has_many :articles
     validates :username, presence: true,
                         uniqueness: { case_sensitive: false },
@@ -6,5 +7,5 @@ class User < ApplicationRecord
     validates :email, presence: true,
                         length: { maximum: 105 },
                         uniqueness: { case_sensitive: false }
-    
+    has_secure_password
 end 
