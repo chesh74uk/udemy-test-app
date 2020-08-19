95af10cfd92b326ff2367e4b077f3c30ee200f16
Chesh <chesh@chesh.info>
2020-08-19 12:50:36 +0000

    added signup and home poage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
    def create
        @user = User.new(user_params)
        if @user.save
-           flash[:notice] = "User signed in"
+           flash[:notice] = "#{@user.username} signed in"
            redirect_to articles_path
        else
            render 'new'
