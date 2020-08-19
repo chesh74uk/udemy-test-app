0f0dacfa3b723eea0949f5d9d02143d2b7e643d2
Chesh <chesh@chesh.info>
2020-08-19 14:31:33 +0000

    added user profiles page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,11 @@ class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update]
     
     def show
+        @articles = @user.articles
+    end
+    
+    def index
+        @users = User.all
     end
    
    def new
