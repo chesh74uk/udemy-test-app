be241926bbede65978335aef01853bf9814df0ae
Chesh <chesh@chesh.info>
2020-08-20 13:52:42 +0000

    added actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, except: [:show, :index]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+    
     
     def show
     end
@@ -17,6 +20,7 @@ class ArticlesController < ApplicationController
     
     def create
         @article = Article.new(article_params)
+        @article_user = current_user
         if @article.save
             flash[:notice] = "Article created"
             redirect_to @article
@@ -52,6 +56,13 @@ class ArticlesController < ApplicationController
     def article_params
         params.require(:article).permit(:title, :description)
     end 
+    
+    def require_same_user
+        if current_user != @article.user_id
+            flash[:alert] = "Not your article, go away"
+            redirect_to @article
+        end
+    end
     
     
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update]
+    before_action :require_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
     
     def show
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
@@ -29,6 +31,7 @@ class UsersController < ApplicationController
    def create
        @user = User.new(user_params)
        if @user.save
+           session[:user_id] = @user.id
            flash[:notice] = "#{@user.username} signed in"
            redirect_to articles_path
        else
@@ -45,5 +48,12 @@ class UsersController < ApplicationController
    def user_params
        params.require(:user).permit(:username, :email, :password)
    end
+   
+   def require_same_user
+        if current_user != @user
+            flash[:alert] = "Not your user, stobbit!"
+            redirect_to @user
+        end
+    end
    
 end
