cae7b6f7a06ba5f67468d0ec176a93284d405788
Chesh <chesh@chesh.info>
2020-08-21 13:05:34 +0000

    added test stuff

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,39 @@
+class CategoriesController < ApplicationController
+    before_action :require_admin, except: [:index, :show]
+    
+    def index
+        @categories = Category.paginate(page: params[:page], per_page: 5)
+    end 
+    
+    def new
+        @category = Category.new
+    end 
+    
+    def show
+        @category = Category.find(params[:id])
+    end
+    
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:success] = "Category created"
+            redirect_to @category
+        else
+            render 'new'
+        end
+    end 
+    
+    private
+    
+    def category_params
+        params.require(:category).permit(:name)
+    end
+    
+    def require_admin
+       if !(logged_in? && current_user.admin?)
+           flash[:danger] = "Only admins can mess about"
+           redirect_to categories_path
+       end
+    end 
+    
+end
