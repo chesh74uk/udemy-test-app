4f7bb60f9c12fd29fafc37dd63eb497bc603c9c4
Chesh <chesh@chesh.info>
2020-08-24 09:11:37 +0000

    added edit categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
     before_action :require_admin, except: [:index, :show]
+    before_action :set_category, only: [:show, :edit, :update]
     
     def index
         @categories = Category.paginate(page: params[:page], per_page: 5)
@@ -10,7 +11,7 @@ class CategoriesController < ApplicationController
     end 
     
     def show
-        @category = Category.find(params[:id])
+        @articles = @category.articles.paginate(page: params[:page], per_page: 5) 
     end
     
     def create
@@ -23,7 +24,23 @@ class CategoriesController < ApplicationController
         end
     end 
     
+    def edit
+    end 
+    
+    def update
+        if @category.update(category_params)
+            flash[:success] = "Updated category"
+            redirect_to @category
+        else
+            render 'edit'
+        end
+    end 
+    
     private
+    
+    def set_category
+        @category = Category.find(params[:id])
+    end
     
     def category_params
         params.require(:category).permit(:name)
